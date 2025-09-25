d14d6a0e98bb65efdec11dc66398cfaca6518ec2
YoelOro81 <54050908+YoelOro81@users.noreply.github.com>
2025-09-25 08:22:58 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚨 Fire Alarm App (MVP)</h1>
+      <p>App base lista para construir tu sistema inteligente.</p>
+    </div>
+  );
+}
